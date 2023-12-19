c532222a4c9abffec93a9398087f85a057f42533
DmytroKondrashov <kondrashovd101@gmail.com>
2023-12-19 19:51:10 +0200

    fix getting data from Mongodb

--- apps/users/src/app/user.service.ts
+++ apps/users/src/app/user.service.ts
@@ -2,18 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Db, MongoClient } from 'mongodb';
 import * as mongo from 'mongodb'
-import { InjectDb } from 'nest-mongodb';
 
 @Injectable()
 export class UserService {
   private readonly collection: mongo.Collection;
 
-  constructor(
-    private readonly configService: ConfigService,
-    @InjectDb() private readonly db: Db
-  ) {
-    this.collection = this.db.collection('users');
-  }
+  constructor(private readonly configService: ConfigService) {}
 
   database: Db;
   client: MongoClient;
@@ -24,27 +18,19 @@ export class UserService {
 
     this.client = new MongoClient(connectionUrl);
     this.database = this.client.db(dbName);
-    await this.client.connect();
   }
 
   async onModuleDestroy() {
     await this.client.close();
   }
 
-  // collection(collectionName: string): Collection<Document> {
-  //   return this.database.collection(collectionName);
-  // }
-
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
 
   async getUsers() {
-    console.log('=============================================')
-    const result = await this.collection.find({}).toArray();
-    console.log(this.db.collection)
-    console.log(this.configService.get('MONGODB_DATABASE_NAME'))
+    const collection = this.database.collection('users');
+    const result = await collection.find({}).toArray();
     return result;  
-    // return 'true';
   }
 }
