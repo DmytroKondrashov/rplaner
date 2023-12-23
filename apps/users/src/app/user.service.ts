2966ae2fcd87cce525cba9be7dc73cbbf292649f
DmytroKondrashov <kondrashovd101@gmail.com>
2023-12-23 20:10:18 +0200

    comment out previous functionality

--- apps/users/src/app/user.service.ts
+++ apps/users/src/app/user.service.ts
@@ -1,36 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Db, MongoClient } from 'mongodb';
+import { ClientProxy } from '@nestjs/microservices';
+// import { Db, MongoClient } from 'mongodb';
 import * as mongo from 'mongodb'
 
 @Injectable()
 export class UserService {
   private readonly collection: mongo.Collection;
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject('USERS_SERVICE') private client: ClientProxy,
+  ) {}
 
-  database: Db;
-  client: MongoClient;
+  // database: Db;
+  // client: MongoClient;
 
-  public async onModuleInit(): Promise<void> {
-    const connectionUrl = this.configService.get('MONGODB_CONNECTOIN_STRING');
-    const dbName = this.configService.get('MONGODB_DATABASE_NAME');
+  // public async onModuleInit(): Promise<void> {
+  //   const connectionUrl = this.configService.get('MONGODB_CONNECTOIN_STRING');
+  //   const dbName = this.configService.get('MONGODB_DATABASE_NAME');
 
-    this.client = new MongoClient(connectionUrl);
-    this.database = this.client.db(dbName);
-  }
+  //   this.client = new MongoClient(connectionUrl);
+  //   this.database = this.client.db(dbName);
+  // }
 
-  async onModuleDestroy() {
-    await this.client.close();
-  }
+  // async onModuleDestroy() {
+  //   await this.client.close();
+  // }
 
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
 
   async getUsers() {
-    const collection = this.database.collection('users');
-    const result = await collection.find({}).toArray();
-    return result;  
+    // const collection = this.database.collection('users');
+    // const result = await collection.find({}).toArray();
+    // return result;
+    try {
+      return this.client.send<string>({cmd: 'findAll'}, {}).toPromise();
+    } catch (error) {
+      console.error('Error in /users request:', error.message);
+    }
   }
 }
