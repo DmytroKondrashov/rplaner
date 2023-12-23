c9a32f92c18a1a831ab7492df2b0266b4b133c4e
DmytroKondrashov <kondrashovd101@gmail.com>
2023-12-23 20:09:56 +0200

    register transport

--- apps/users/src/app/user.module.ts
+++ apps/users/src/app/user.module.ts
@@ -4,9 +4,22 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongoModule } from 'nest-mongodb';
 import { ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [MongoModule.forRoot('mongodb://localhost', 'rplaner')],
+  // imports: [MongoModule.forRoot('mongodb://localhost', 'rplaner')],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'users_queue',
+        },
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, ConfigService],
 })
