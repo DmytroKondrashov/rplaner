a8e5ce405b2d6be6db4a07db8ff81522f22ae7c5
DmytroKondrashov <kondrashovd101@gmail.com>
2023-12-23 19:22:05 +0200

    register mongo_queue

--- apps/mongo/src/main.ts
+++ apps/mongo/src/main.ts
@@ -1,22 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { MongoModule } from 'nest-mongodb';
 
-import { AppModule } from './app/mongo.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(MongoModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'mongo_queue',
+    },
+  });
+  await app.listen();
 }
 
 bootstrap();
