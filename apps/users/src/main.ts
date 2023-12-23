3acc3b02ea97b40077965a31d0d62cdc1eadf28a
DmytroKondrashov <kondrashovd101@gmail.com>
2023-12-23 19:18:00 +0200

    remoce unneeded code

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -4,14 +4,6 @@ import { NestFactory } from '@nestjs/core';
 import { UserModule } from './app/user.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // const globalPrefix = 'api';
-  // app.setGlobalPrefix(globalPrefix);
-  // const port = process.env.PORT || 3000;
-  // await app.listen(port);
-  // Logger.log(
-  //   `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  // );
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(UserModule, {
     transport: Transport.RMQ,
     options: {
