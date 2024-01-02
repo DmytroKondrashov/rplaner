9062b5d50b23922cb490655d9f53254653a936b5
DmytroKondrashov <kondrashovd101@gmail.com>
2024-01-02 16:44:40 +0200

    return users list

--- apps/users/src/app/user.service.ts
+++ apps/users/src/app/user.service.ts
@@ -1,27 +1,50 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ClientProxy } from '@nestjs/microservices';
+// import { ClientProxy } from '@nestjs/microservices';
 // import { Db, MongoClient } from 'mongodb';
-import * as mongo from 'mongodb'
+import {
+  Collection,
+  Db,
+  MongoClient,
+} from 'mongodb';
 
 @Injectable()
 export class UserService {
-  private readonly collection: mongo.Collection;
+  database: Db;
+  client: MongoClient;
 
   constructor(
     private readonly configService: ConfigService,
-    @Inject('MONGO_SERVICE') private client: ClientProxy,
+    // @Inject('MONGO_SERVICE') private client: ClientProxy,
   ) {}
 
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  public async onModuleInit(): Promise<void> {
+    const connectionUrl = this.configService.get('MONGODB_CONNECTOIN_STRING');
+    const dbName = this.configService.get('MONGODB_DATABASE_NAME');
+
+    this.client = new MongoClient(connectionUrl);
+    this.database = this.client.db(dbName);
+  }
+
+  async onModuleDestroy() {
+    await this.client.close();
+  }
+
+  collection(collectionName: string): Collection<Document> {
+    return this.database.collection(collectionName);
+  }
+
+  findAll(collection: string): unknown {
+    return this.collection(collection).find().toArray();
   }
 
   async getUsers() {
-    try {
-      return this.client.send<string>({cmd: 'findAll'}, {}).toPromise();
-    } catch (error) {
-      console.error('Error in /users request:', error.message);
-    }
+    // This is not working for now, maybe refactor later
+    // try {
+    //   return this.client.send<string>({cmd: 'findAll'}, {}).toPromise();
+    // } catch (error) {
+    //   console.error('Error in /users request:', error.message);
+    // }
+    return this.findAll('users');
   }
 }
