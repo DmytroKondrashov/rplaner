bb923a3124978943a9e567067cbfd9546d6b1e3d
DmytroKondrashov <kondrashovd101@gmail.com>
2024-01-04 20:30:44 +0200

    add validation pipe

--- apps/client/src/app/dtos/sign.in.dto.ts
+++ apps/client/src/app/dtos/sign.in.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class SignInDto {
   @ApiProperty()
   @IsString()
-  userName: string;
+  @IsEmail()
+  email: string;
 
   @ApiProperty()
   @IsString()
--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,15 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { ClientModule } from './app/client.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ClientModule);
+  app.useGlobalPipes(new ValidationPipe());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
